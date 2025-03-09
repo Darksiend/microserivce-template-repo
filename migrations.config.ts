982b7278c25d22a363406068227d26e2f01a9ce7
Anton Chesnokov <logosmk@icloud.com>
2025-03-09 14:19:33 +0200

    init

--- migrations.config.ts
+++ migrations.config.ts
@@ -1,16 +1,16 @@
 import 'dotenv/config';
-import { ConfigService } from '@nestjs/config';
-import { DataSource } from 'typeorm';
+import {ConfigService} from '@nestjs/config';
+import {DataSource} from 'typeorm';
 
 const configService = new ConfigService();
 
 export default new DataSource({
-  type: 'postgres',
-  host: configService.get('DATABASE_HOST'),
-  port: configService.get('DATABASE_PORT'),
-  username: configService.get('DATABASE_USER'),
-  password: configService.get('DATABASE_PASSWORD'),
-  database: configService.get('DATABASE_NAME'),
-  entities: ['dist/**/*.entity{.ts,.js}'],
-  migrations: ['dist/migrations/*{.ts,.js}'],
+    type: 'postgres',
+    host: configService.get('DATABASE_HOST'),
+    port: configService.get('DATABASE_PORT'),
+    username: configService.get('DATABASE_USER'),
+    password: configService.get('DATABASE_PASSWORD'),
+    database: configService.get('DATABASE_NAME'),
+    entities: ['dist/**/*.entity{.ts,.js}'],
+    migrations: ['dist/migrations/*{.ts,.js}'],
 });
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,19 @@
-import { Module } from '@nestjs/common';
+import {Module} from '@nestjs/common';
 import {ConfigModule} from "@nestjs/config";
 import {TypeOrmModule} from "@nestjs/typeorm";
 import {TypeOrmConfigService} from "./config/typeorm-config.service";
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      envFilePath: '.env',
-      isGlobal: true,
-    }),
-      TypeOrmModule.forRootAsync({
-    useClass: TypeOrmConfigService,
-  }),],
-  controllers: [],
-  providers: [],
+    imports: [
+        ConfigModule.forRoot({
+            envFilePath: '.env',
+            isGlobal: true,
+        }),
+        TypeOrmModule.forRootAsync({
+            useClass: TypeOrmConfigService,
+        }),],
+    controllers: [],
+    providers: [],
 })
-export class AppModule {}
+export class AppModule {
+}
