a0de0bd6d0b7903c9eacde41ad14e7b43d515fe8
Anton Chesnokov <30446099+Darksiend@users.noreply.github.com>
2025-04-20 19:07:22 +0300

    refactor: format code for consistency and readability

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,26 @@
-import {NestFactory} from '@nestjs/core';
-import {AppModule} from "./app.module";
+import { NestFactory } from '@nestjs/core';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
+import { join } from 'path';
+import { AppModule } from './app.module';
 
 async function bootstrap() {
-    const app = await NestFactory.create(AppModule);
-    await app.listen(process.env.PORT ?? 3001);
+  const app = await NestFactory.create(AppModule);
+
+  const grpcServer = await NestFactory.createMicroservice<MicroserviceOptions>(
+    AppModule,
+    {
+      transport: Transport.GRPC,
+      options: {
+        url: '0.0.0.0:50051',
+        package: 'auth',
+        protoPath: join(__dirname, 'common/proto/auth.proto'),
+      },
+    },
+  );
+
+  await grpcServer.listen();
+
+  await app.listen(process.env.PORT ?? 3001);
 }
 
 bootstrap();
